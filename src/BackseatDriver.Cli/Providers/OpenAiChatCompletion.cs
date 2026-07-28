3ee4b29b57fecf302fe452a5cd7a48bca41bdb2c
Philipp Kiener <kiener.phil@gmail.com>
2026-07-28 21:12:50 +0200

    Use a handful of system messages

--- src/BackseatDriver.Cli/Providers/OpenAiChatCompletion.cs
+++ src/BackseatDriver.Cli/Providers/OpenAiChatCompletion.cs
@@ -16,6 +16,11 @@ public sealed class OpenAiChatCompletion(Uri baseAddress) : IProvider
     {
         ChatMessage[] chatMessages = [
             new("system", "You are a simple agent. You respond in the shortest possible way, exactly in the format you're told to adhere to."),
+            new("system", "If you need to access data that you do not have, you can use one of the given tools."),
+            new("system", "You can use a tool called 'cat' to read the contents of a file on disk. Invoke it as 'cat $path' to read the contents of the file located at $path."),
+            new("system", "You can use a tool called 'ls' to list all contents of a directory. Invoke it as 'ls $path' to list the contents of the directory located at $path."),
+            new("system", "You are allowed to ask one or more clarifying questions to the user before answering their prompt. Keep it short and succint, feel free to be blunt."),
+            new("system", "Prefix your answer with 'RESPONSE: ' if you intend to answer the prompt. Prefix your answer with 'TOOL: ' if you need to invoke a tool to provide an answer. Prefix your answer with 'PROMPT: ' if you need to ask the user a clarifying question to proceed."),
             ..messages.Where(static m => !string.IsNullOrWhiteSpace(m)).Select(static m => new ChatMessage("user", m))
         ];
 

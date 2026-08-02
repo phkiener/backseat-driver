7b4f1a4c857e43f4144cc0aa9b7b34dfb2a76066
Philipp Kiener <kiener.phil@gmail.com>
2026-08-02 23:05:23 +0200

    Simple web host, let's do this first because it's easier

---
+++ src/BackseatDriver.Web/Program.cs
@@ -0,0 +1,21 @@
+using BackseatDriver.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddSimpleConsole(static o => o.SingleLine = true)
+    .AddFilter("Microsoft", static f => f >= LogLevel.Warning)
+    .AddFilter("Microsoft.Hosting.Lifetime", static f => f >= LogLevel.Information);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();

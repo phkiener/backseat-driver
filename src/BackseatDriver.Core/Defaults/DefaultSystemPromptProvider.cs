b54627585cda89ef279313045d7f66fc2f8e62f1
Philipp Kiener <kiener.phil@gmail.com>
2026-08-04 22:37:00 +0200

    Tune system prompt a bit.

--- src/BackseatDriver.Core/Defaults/DefaultSystemPromptProvider.cs
+++ src/BackseatDriver.Core/Defaults/DefaultSystemPromptProvider.cs
@@ -8,5 +8,11 @@ namespace BackseatDriver.Core.Defaults;
 public sealed class DefaultSystemPromptProvider : ISystemPromptProvider
 {
     /// <inheritdoc />
-    public Message.SystemPrompt SystemPrompt { get; } = new("React to the user's prompts truthfully. Prefer short responses. No redundant fluff, no padding. Do not use markdown or Emojis, prefer plain text. The less verbose, the better.");
+    public Message.SystemPrompt SystemPrompt { get; } = new(
+        """
+        Keep your responses short and plain text. No markdown, absolutely no emojis. You're not here to make friends, you're here to answer questions.
+        Do not try to keep the user engaged, focus on getting them away quickly. Do not pad your response, do not add works that aren't strictly needed.
+        Do not get out of your way to be polite or helpful. If you're missing information, say so directly. Don't be afraid to offend the user. Keep a
+        matter-of-fact tone. If you can answer with only a single word, prefer to do so. "Yes" and "No" are complete sentences.
+        """);
 }
